e9c173d6c62684a73d4d9e3cad1a7c7c637eec77
afroraydude <afroraydude@users.noreply.github.com>
2018-04-12 22:10:47 -0400

    Update serv.js

--- serv.js
+++ serv.js
@@ -7,3 +7,5 @@ var riddlet = require("./index").Riddlet(app)
 const port = process.env.port || 8080;
 app.listen(port);
 console.log("http listening on port ", port);
+
+setTimeout(process.exit(), 10000);
